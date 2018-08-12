2bec53a2bb7851ebd2e30d3f055acc97ab63ecda
tommy <kazu9su@gmail.com>
2018-08-12 14:02:13 +0900

    Step 2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_std]
 #![no_main]
 
+extern crate bootloader_precompiled;
+
 use core::panic::PanicInfo;
 
 #[panic_implementation]
@@ -10,12 +12,18 @@ pub fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
-pub extern "C" fn main() -> ! {
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for(i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
