3bda411e0337bb1e5ce4ad18381f4cd988981270
tommy <kazu9su@gmail.com>
2018-08-26 21:53:49 +0900

    20180826

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 #![feature(panic_implementation)]
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
 extern crate bootloader_precompiled;
 extern crate volatile;
 #[macro_use]
 extern crate lazy_static;
 extern crate spin;
+#[cfg(test)]
+extern crate std;
+#[cfg(test)]
+extern crate array_init;
 
 use core::panic::PanicInfo;
 
 #[macro_use]
 mod vga_buffer;
 
+#[cfg(not(test))] // only compile when the test flag is not set
 #[panic_implementation]
 #[no_mangle]
 pub fn panic(info: &PanicInfo) -> ! {
@@ -22,9 +28,11 @@ pub fn panic(info: &PanicInfo) -> ! {
 
 static HELLO: &[u8] = b"Hello World!";
 
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    vga_buffer::print_something();
+    //panic!("Some panic message");
     loop {}
 }
