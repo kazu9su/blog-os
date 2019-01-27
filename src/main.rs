c743968a1b924b850ba87af637bb027042ba69f4
tommy <kazu9su@gmail.com>
2019-01-27 18:22:21 -0500

    Finish chapter integration test

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,13 @@
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
-extern crate bootloader_precompiled;
-extern crate volatile;
+#[macro_use]
+extern crate blog_os;
+extern crate x86_64;
 #[macro_use]
 extern crate lazy_static;
-extern crate spin;
-#[cfg(test)]
-extern crate std;
-#[cfg(test)]
-extern crate array_init;
 
 use core::panic::PanicInfo;
-
-#[macro_use]
-mod vga_buffer;
 
 #[cfg(not(test))] // only compile when the test flag is not set
 #[panic_implementation]
@@ -28,11 +21,12 @@ pub fn panic(info: &PanicInfo) -> ! {
 
 static HELLO: &[u8] = b"Hello World!";
 
+#[cfg(not(feature = "integration-test"))] // new
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-    vga_buffer::print_something();
-    //panic!("Some panic message");
+    println!("Hello World{}", "!"); // prints to vga buffer
+
+    // normal execution
     loop {}
 }
